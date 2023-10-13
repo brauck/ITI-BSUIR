d18bb2b5e11947ecd5e9a73e5548546970d6c9af
brauck <brauckhoff@yandex.com>
2023-10-13 07:08:00 +0300

    solve variant 2

--- ОАПЯВУ/lab1_Линейные_алгоритмы/task1/task1/task1.cpp
+++ ОАПЯВУ/lab1_Линейные_алгоритмы/task1/task1/task1.cpp
@@ -7,6 +7,13 @@
 
 using namespace std;
 
+int variant = 0;
+
+void test(double z1, double z2) {
+	variant++;
+	cout << "Variant " << variant << ":\nz1 = " << z1 << "\nz2 = " << z2 << "\n\n";
+}
+
 int main() {
 	// setlocale(LC_CTYPE, "Russian");	// русификации консоли для ВЫВОДА
 	SetConsoleCP(1251);
@@ -17,6 +24,7 @@ int main() {
 	double  z1 = 0.0;
 	double z2 = 0.0;
 
+	// variant 1
 	// single sin(), cos()
 	double ssin = sin(3 * M_PI - 2 * a);
 	double scos = cos(5 * M_PI + 2 * a);
@@ -24,7 +32,13 @@ int main() {
 	z1 = 2 * ssin * ssin * scos * scos;
 	z2 = (1 / 4.0) - (1 / 4.0) * sin((5 / 2.0) * M_PI - 8 * a);
 
-	cout << z1 << '\n' << z2;
+	test(z1, z2);
+
+	// variant 2
+	z1 = cos(a) + sin(a) + cos(3 * a) + sin(3 * a);
+	z2 = 2 * pow(2, 0.5) * cos(a) * sin(M_PI / 4 + 2 * a);
+	
+	test(z1, z2);
 	
 	return 0;
 }
