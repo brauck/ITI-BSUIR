6d8c57f8beef82aa1e9a7549120dbc85a22d924f
brauck <brauckhoff@yandex.com>
2023-12-30 19:02:15 +0300

    solve lab1.cpp

--- ОАПЯВУ/lab1_Линейные_алгоритмы/variant4/variant4/variant_4.cpp
+++ ОАПЯВУ/lab1_Линейные_алгоритмы/variant4/variant4/variant_4.cpp
@@ -43,16 +43,18 @@ int main()
 
 	// Задание 2
 	cout << "Задание 2" << endl;
-	double x = 4e1;
+	double x = 4e3;
 	double y = -0.875;
-	double z = 4.75e-4;
+	double z = -4.75e-4;
 	double w = 1.9873;
-
+	
 	double step = fabs(cos(x) - cos(y));
 	step = pow(step, 1 + 2 * pow(sin(y), 2));
 	step = step * (1 + z + (pow(z, 2) / 2) + (pow(z, 3) / 3) + (pow(z, 4) / 4));
-	cout << step << endl;
 	//cout << typeid(x).name() << endl;
+	printf("%-22s %.4f \n", "Ожидаемый результат:", w);
+	printf("%-22s %.4f \n", "Полученный результат:", step);
+
 	system("pause");
 	return 0;
 }
