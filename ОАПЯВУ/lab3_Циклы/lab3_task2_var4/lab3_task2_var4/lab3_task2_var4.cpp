c09af57e7307f7e234d29457a73c7379a6ca5d7e
brauck <brauckhoff@yandex.com>
2024-01-08 01:30:39 +0300

    do lab3 task2

---
+++ ОАПЯВУ/lab3_Циклы/lab3_task2_var4/lab3_task2_var4/lab3_task2_var4.cpp
@@ -0,0 +1,38 @@
+﻿// Lab #3
+// task #2
+// variant #4
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void main()
+{
+	bool isSpace = true;
+	int n{};
+	cout << "Введите число от 0 до 20: ";
+	cin >> n;
+	if (n < 0 || n > 20)
+	{
+		cout << "Число должно быть в диапозоне от 0 до 20" << endl;
+		system("pause");
+		return;
+	}
+	int count = n;
+
+	for (int i = 0, k = 0; i < n; i++, k++)
+	{
+        
+		for (int j = 0; j < n; j++)
+		{
+			count--;
+			if (!count) isSpace = false;
+			if (isSpace) cout << ' ';
+			else cout << '*';
+		}
+		count = n - k - 1;
+		isSpace = true;
+		cout << "\n";
+	}
+
+	system("pause");	
+}
