7f0850621c7d5b6eee95b3ab6aa33b2d047b5069
brauck <109923715+brauck@users.noreply.github.com>
2025-02-19 00:32:15 +0300

    make console reverse

--- sp_labs/sources_2/lab2-5/FileReversSol/FileRevers/resource.h
+++ sp_labs/sources_2/lab2-5/FileReversSol/FileRevers/resource.h
@@ -1,26 +1,25 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by SpLb24_IClient.rc
+// Used by FileRevers.rc
 //
 #define IDC_MYICON                      2
-#define IDD_SPLB24ICLIENT_DIALOG        102
+#define IDD_FILEREVERS_DIALOG           102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
 #define IDM_EXIT                        105
-#define IDI_SPLB24ICLIENT               107
-#define IDI_SMALL                       108
-#define IDC_SPLB24ICLIENT               109
+#define IDM_OPENFILE                    32771
+#define IDC_FILEREVERS                  109
 #define IDR_MAINFRAME                   128
+#define IDI_ICON1                       130
 #define IDC_STATIC                      -1
-#define IDM_FILE_DLL                    32771
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32772
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
---
+++ sp_labs/sources_2/reverse/reverse.cpp
@@ -0,0 +1,60 @@
+// reverse.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const char base[300] = "14. Rearrange the characters in each word in reverse order and write the resulting words to the end of the file (to reverse words, you can use the Windows library functions \"_strrev, _tcsrev, _mbsrev\").";
+    cout << base << "\n\n";
+
+    char word[30] = { 0 };
+    char tempWord[30] = { 0 };
+
+    int j = 0;
+    int wLen = 0;
+
+    for (int i = 0; i < strlen(base); i++)
+    {
+        if (!isalpha(base[i]) && !strlen(word))
+        {
+            continue;
+        }
+
+        if (!isalpha(base[i]) && strlen(word))
+        {
+            wLen = strlen(word);
+            for (int k = 0; k < wLen; k++)
+            {
+                tempWord[k] = word[wLen - k -1];
+            }
+            cout << tempWord << ' ';
+            j = 0;
+            wLen = 0;
+            memset(word, 0, sizeof(word));
+            memset(tempWord, 0, sizeof(tempWord));
+            continue;
+        }
+
+        if (isalpha(base[i]))
+        {
+            word[j] = base[i];
+            j++;
+        }
+    }
+
+    system("pause");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
